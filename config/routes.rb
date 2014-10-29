68affd84fad7076c9794619ff332fba612cf0e91
Sergey Navanov <nomadser@gmail.com>
2014-10-29 13:40:57 +0000

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :users
+   resources :sessions, only: [:new, :create, :destroy]
+  
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   
   get 'users/new'
 
